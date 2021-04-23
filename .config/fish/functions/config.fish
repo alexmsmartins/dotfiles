6f5323fd7769c1b3bb2ee4f1cb32f4d5ebd1bc1c
Alexandre Martins <alexmsmartins@gmail.com>
2021-04-23 17:41:59 +0100

    Link so that it is easier to discover instructions and setup a new computer.

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,4 +1,5 @@
-# Defined in /var/folders/ny/cxvbndcx03sb0yrbx6hd35j8qslqdf/T//fish.rxGvUk/config.fish @ line 2
+# Defined in /var/folders/ny/cxvbndcx03sb0yrbx6hd35j8qslqdf/T//fish.YxLHnD/config.fish @ line 2
 function config
+  # https://www.atlassian.com/git/tutorials/dotfiles
   git --git-dir={$HOME}/.cfg/ --work-tree={$HOME} $argv
 end
