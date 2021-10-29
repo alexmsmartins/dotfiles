9a20084b54fc9d603ff7d9208e6728c39961d167
Alexandre Martins <alexmsmartins@gmail.com>
2021-10-29 09:35:06 +0100

    Switch to fzf from Homebrew.

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/amartins/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/amartins/.fzf/bin"
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/amartins/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/amartins/.fzf/shell/key-bindings.bash"
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
