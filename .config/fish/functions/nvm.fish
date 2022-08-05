b0c4b5f7cf19c61c58c49fa0e578cec9fd80f4c3
Alexandre Martins <alexmsmartins@gmail.com>
2022-08-05 14:38:06 +0100

    TMUX prefix inside TMUX (tested with a remote session) can now be sent from the outer tmux instance.
    Nvm now dowloads properly if not installed in the home folder yet for fish shell.

--- .config/fish/functions/nvm.fish
+++ .config/fish/functions/nvm.fish
@@ -1,5 +1,6 @@
 function nvm
-  if test ! -e ~/.nvm/mvn.sh
+  if test ! -e ~/.nvm/nvm.sh
+    echo "Installing nvm from github"
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
   end
   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
