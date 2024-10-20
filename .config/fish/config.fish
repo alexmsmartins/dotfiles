6dc7d294c27d46b3df314d1a77da819a10c93c79
Alexandre Martins <alexmsmartins@gmail.com>
2024-10-20 19:30:11 +0100

    Several changes from the Work MBP including sdkman, git, fish plugins, etc. (#38)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -139,6 +139,7 @@ export TASKDDATA=/var/taskd
 # Local and private configuration
 source $HOME/.config/fish/config_local.fish
 
-
+# SDKMAN setup
+fish_add_path (find ~/.sdkman/candidates/*/current/bin -maxdepth 0)
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/amartins/.ghcup/bin $PATH # ghcup-env
